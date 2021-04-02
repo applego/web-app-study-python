b55205aa31352a816f34d96752f7f1a46ae713ea
applego <applegorillappa@gmail.com>
2021-04-02 22:41:18 +0900

    💡リファクタ 　関数の切り出し(views.py）

--- workerthread.py
+++ workerthread.py
@@ -1,15 +1,12 @@
-
-
-from datetime import datetime
 import os
-import socket
+import re
 import traceback
+from datetime import datetime
+from socket import socket
 from threading import Thread
 from typing import Optional, Tuple
-import textwrap
-from pprint import pformat
-import re
-import urllib.parse
+
+import views
 
 class WorkerThread(Thread):
   # 実行ファイルのあるディレクトリ
@@ -58,72 +55,12 @@ class WorkerThread(Thread):
 
       # pathが/nowのときは、現在時刻を表示するHTMLを生成する
       if path == "/now":
-        html = f"""\
-          <html>
-          <body>
-            <h1>Now: {datetime.now()}</h1>
-          </body>
-          </html>
-          """
-        response_body = textwrap.dedent(html).encode()
-
-        # Content-Typeを指定
-        content_type = "text/html; charset=UTF-8"
-
-        #　レスポンスラインを生成
-        response_line = "HTTP/1.1 200 OK\r\n"
-
+        response_body, content_type, response_line = views.now()
       # pathが/nowのときは、現在時刻を表示するHTMLを生成する
       elif path == "/show_request":
-        html = f"""\
-          <html>
-          <body>
-            <h1>Request Line:</h1>
-            <p>
-              {method} {path} {http_version}
-            </p>
-            <h1>Headers:</h1>
-            <pre>
-              {pformat(request_header)}
-            </pre>
-            <h1>Body:</h1>
-            <pre>
-              {request_body.decode("utf-8","ignore")}
-            </pre>
-          </body>
-          </html>
-          """
-        response_body = textwrap.dedent(html).encode()
-
-        # Content-Typeを指定
-        content_type = "text/html; charset=UTF-8"
-
-        #　レスポンスラインを生成
-        response_line = "HTTP/1.1 200 OK\r\n"
-
+        response_body, content_type, response_line = views.show_request(method, path, http_version, request_header, request_body)
       elif path == "/parameters":
-        if method == "GET":
-          response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-          content_type = "text/html; charset=UTF-8"
-          response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
-
-        elif method == "POST":
-          post_params = urllib.parse.parse_qs(request_body.decode())
-          html = f"""\
-            <html>
-            <body>
-              <h1>Parameters:</h1>
-              <pre>{pformat(post_params)}</pre>
-            </body>
-            </html>
-          """
-          response_body = textwrap.dedent(html).encode()
-
-          # Content-Typeを指定
-          content_type = "text/html; charset=UTF-8"
-
-          # レスポンスラインを生成
-          response_line = "HTTP/1.1 200 OK\r\n"
+        response_body, content_type, response_line = views.parameters(method, request_body)
 
       # pathがそれ以外のときは、静的ファイルからレスポンスを生成する
       else:
