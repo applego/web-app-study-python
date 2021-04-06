7965b99adfe678b75f1f9643aec71548017a0634
applego <applegorillappa@gmail.com>
2021-04-07 00:14:25 +0900

    STEP3: Webアプリケーション内でCookieを使いやすくする

--- henango/http/request.py
+++ henango/http/request.py
@@ -3,14 +3,24 @@ class HTTPRequest:
   method: str
   http_version: str
   headers: dict
+  cookies: dict
   body: bytes
   params: dict
 
   def __init__(
-    self, path:str,method:str,http_version:str,headers:dict,body:bytes,params:dict = None
+    self,
+    path: str,
+    method: str,
+    http_version: str,
+    headers: dict = None,
+    cookies: dict = None,
+    body: bytes = b"",
+    params:dict = None,
     ):
     if headers is None:
       headers = {}
+    if cookies is None:
+      cookies = {}
     if params is None:
       params = {}
 
@@ -18,5 +28,6 @@ class HTTPRequest:
     self.method = method
     self.http_version = http_version
     self.headers = headers
+    self.cookies = cookies
     self.body = body
     self.params = params
